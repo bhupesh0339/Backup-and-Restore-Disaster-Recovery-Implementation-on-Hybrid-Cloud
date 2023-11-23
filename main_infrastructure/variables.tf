023f5ea4f96eb71bfb24eb857003844a7b0015de
Bhupesh Yadav <ethan@Ethans-Air.cust.communityfibre.co.uk>
2023-11-23 01:54:17 +0000

    Added Gitignore

--- main_infrastructure/variables.tf
+++ main_infrastructure/variables.tf
@@ -13,7 +13,8 @@ variable "database_name" {
   sensitive = true
 }
 variable "certbot-ssl-email" {}
-variable "git_repository_python_app" {}
+variable "git_repo_https_url" {}
+variable "git_repo_application_branch" {}
 variable "github_token" {}
 variable "azure-uploadfile-account-username" {}
 variable "azure-uploadfile-account-password" {}
